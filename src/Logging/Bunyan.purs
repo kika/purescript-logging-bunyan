37f276d5f1db242db6847f11caa3d44e749a6fa9
Kirill Pertsev <kika@rackmaze.com>
2016-05-10 04:28:34 -0700

    Cosmetics

--- src/Logging/Bunyan.purs
+++ src/Logging/Bunyan.purs
@@ -24,31 +24,30 @@ foreign import data LOG :: !
 foreign import data Logger :: *
 
 -- Foreign implementations
-foreign import createLoggerImpl :: Fn1 Foreign Logger
-foreign import infoImpl :: 
+foreign import createLoggerImpl::Fn1 Foreign Logger
+foreign import infoImpl:: 
 -- WHY???  forall a eff. (Log a) => Fn2 Logger a (Eff (log :: LOG | eff) Unit) 
   forall a eff. Fn2 Logger a (Eff (log :: LOG | eff) Unit) 
 
 -- | Phantom type for Options
 foreign import data BunyanOpts :: *
 
-
 -- | `create` options
-name :: Option BunyanOpts String
+name::Option BunyanOpts String
 name = opt "name"
 
 -- | Creates a logger instance based on Bunyan options passed
-create :: Options BunyanOpts -> Logger
+create::Options BunyanOpts -> Logger
 create opts = runFn1 createLoggerImpl $ options opts
 
 class Log entry where
-  info :: forall e. Logger -> entry -> Eff (log :: LOG | e) Unit
+  info::forall e. Logger -> entry -> Eff (log :: LOG | e) Unit
 
-instance infoString :: Log String where
+instance infoString::Log String where
   info logger msg = runFn2 infoImpl logger msg
-instance infoChar :: Log Char where
+instance infoChar::Log Char where
   info logger msg = runFn2 infoImpl logger msg
-instance infoInt :: Log Int where
+instance infoInt::Log Int where
   info logger msg = runFn2 infoImpl logger msg
-instance infoNumber :: Log Number where
+instance infoNumber::Log Number where
   info logger msg = runFn2 infoImpl logger msg
